8b0f418358a16b673685f4cb22c8dc14b5c7c082
LEONARDO FABIO CANO BARRERA <148526592+leocab1@users.noreply.github.com>
2025-10-06 12:47:25 -0600

    Create Frase.js

---
+++ Frase.js
@@ -0,0 +1,6 @@
+ document.addEventListener('DOMContentLoaded', () => {
+    const quote = document.getElementById('quote-container');
+    setTimeout(() => {
+      quote.classList.add('visible');
+    }, 100); 
+  });
